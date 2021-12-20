ec964e594f1b41007931eeba42920b9a1543f612
DianeSimplon <86324298+DianeSimplon@users.noreply.github.com>
2021-12-20 07:35:10 +0100

    Configured for deployment on Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web : gunicorn Bien_immobilier.wsgi
---
+++ data_view/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.db.models.base import Model
+
+# Create your models here.
+#Creer un model qui va herite le model de django
+from upload_csv import models
